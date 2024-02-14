afccb36359ce80c541f4874e3520b9d41057ea50
Hrishikesh Nepal <nepal.hrishikesh@gmail.com>
2024-02-14 00:17:41 -0700

    updated with header and footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 import ListDriversComponent from './components/ListDriversComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <ListDriversComponent />
+      <FooterComponent />
     </div>
   );
 }
